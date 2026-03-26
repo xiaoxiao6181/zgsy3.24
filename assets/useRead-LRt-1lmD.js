066630498e958a1bbd983dba870089c5b3d13960
xiaoxiao6181 <167971162+xiaoxiao6181@users.noreply.github.com>
2026-03-26 19:49:26 +0800

    Delete assets directory

--- assets/useRead-LRt-1lmD.js
+++
@@ -1,1 +0,0 @@
-import{S as s,r as a,T as o}from"./index-CwYflfF_.js";const n=s("notifyList",()=>{const t=a();return{notifyList:o({get:()=>t.value,set:e=>{t.value=e}}),_notifyList:t}},{persist:!0}),u=s("read",()=>{const t=a();return{data:o({get:()=>t.value,set:e=>{t.value=e}}),_data:t}},{persist:!0});export{u as a,n as u};
