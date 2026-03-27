204ded9a98811c26172ac841af93acf9440c9e63
xiaoxiao6181 <167971162+xiaoxiao6181@users.noreply.github.com>
2026-03-27 20:02:05 +0800

    Delete assets directory

--- assets/useNotify-OALtuzbF.js
+++
@@ -1,1 +0,0 @@
-import{S as o,r as s,T as r}from"./index-C5C5p6Yo.js";const i=o("notify",()=>{const t=s();return{notify:r({get:()=>t.value,set:e=>{t.value=e}}),_notify:t}},{persist:!0});export{i as u};
--- assets/useYuEBao-DopDPdeJ.js
+++
@@ -1,1 +0,0 @@
-import{S as e,r as o,T as s}from"./index-C5C5p6Yo.js";const d=e("YuEBao",()=>{const t=o(void 0);return{data:s({get:()=>t.value,set:a=>{t.value=a}}),_data:t}},{persist:!0});export{d as u};
